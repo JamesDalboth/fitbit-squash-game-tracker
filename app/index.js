23edb7b594bd47499fb30e5a0f9b0e4ba65de4ff
JamesDalboth <james.dalboth@gmail.com>
2023-05-20 17:09:09 +0100

    feat: add increment and reset logic
    [basic-app]

--- app/index.js
+++ app/index.js
@@ -1,1 +1,21 @@
-console.log('Hello world!');
+import * as document from "document";
+
+const meButton = document.getElementById("me-button");
+const opButton = document.getElementById("op-button");
+const resetButton = document.getElementById("reset-button");
+
+const incrementScore = (scoreLabel) => {
+  const currentScore = parseInt(scoreLabel.text);
+  scoreLabel.text = currentScore + 1;
+}
+
+const resetScores = () => {
+  meButton.text = "0";
+  opButton.text = "0";
+}
+
+meButton.addEventListener("click", () => incrementScore(meButton));
+opButton.addEventListener("click", () => incrementScore(opButton));
+resetButton.addEventListener("click", () => resetScores());
+
+resetScores();
